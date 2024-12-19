890f0a5d4c5cfcb7668e0c8076d3b3fb50bfeadb
HRITHIK VERMA <verma02hrithik@gmail.com>
2024-12-19 12:15:09 +0530

    Update apiConstants.dart

--- lib/utils/apiConstants.dart
+++ lib/utils/apiConstants.dart
@@ -1,5 +1,4 @@
 class ApiConstants {
-  static String BASE_URL = "https://164.100.196.159/discom_api/api/";
-  static String LOCAL_URL = "http://10.115.35.195/api/";
-  static String GetTransformerByQR = "Transformer/GetTransformerByQR";
+  static String BASE_URL = "https://codeskulptor-demos.commondatastorage.googleapis.com/";
+  static String LOCAL_URL = "descent/background%20music.mp3";
 }
